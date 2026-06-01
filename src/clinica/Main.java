5f896085c7ca52d47f8c6d56c5b703e453b1d191
hmdev2 <hmdev2@proton.me>
2026-06-01 09:00:41 -0300

    feat: Cria classes BaseDAO, PacienteDAO com método findAll e método testePacienteDAOFindAll na Main

--- src/clinica/Main.java
+++ src/clinica/Main.java
@@ -1,13 +1,16 @@
 package clinica;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 import clinica.connection.DatabaseConnection;
+import clinica.dao.PacienteDAO;
 
 public class Main {
 
 	public static void main(String[] args) {
-		testeConexaoDB();
+		testePacienteDAOFindAll();
 	}
 	
 	public static void testeConexaoDB() {
@@ -19,6 +22,18 @@ public class Main {
             System.out.println("Erro:");
             e.printStackTrace();
         }
+	}
+	
+	public static void testePacienteDAOFindAll() {
+		try {
+			PacienteDAO dao = new PacienteDAO();
+			List<Map<String, Object>> pacientes = dao.findAll();
+			for (Map<String, Object> p : pacientes) {
+				System.out.println(p);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
