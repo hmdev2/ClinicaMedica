9fd9b199fad0192d28c8558009ed6264bcc6db82
Kaio <kaiorosalescosta@gmail.com>
2026-06-01 17:52:00 -0300

    Adicionando colaborador

---
+++ src/main/java/clinica/dao/ColaboradorDAO.java
@@ -0,0 +1,46 @@
+package clinica.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import clinica.connection.DatabaseConnection;
+
+public class ColaboradorDAO extends BaseDAO {
+	
+	public void save(String nome, String sobrenome, String cpf) {
+		String sql = "INSERT INTO colaborador (nome, sobrenome, cpf) VALUES (?, ?, ?);";
+		try (
+				Connection con = DatabaseConnection.connect();
+				PreparedStatement ps = con.prepareStatement(sql);
+			) {
+			
+			ps.setString(1, nome);
+            ps.setString(2, sobrenome);
+            ps.setString(3, cpf);
+            ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Map<String, Object>> findAll() throws SQLException{
+		String sql = "SELECT id, nome, sobrenome, cpf FROM colaborador ORDER BY nome, sobrenome";
+		List<Map<String, Object>> colaboradores = new ArrayList<Map<String, Object>>();
+		try (
+				Connection con = DatabaseConnection.connect();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();
+			) {
+			while (rs.next()) {
+				colaboradores.add(lineForMap(rs));
+			}
+		}
+		
+		return colaboradores;
+	}
+}
