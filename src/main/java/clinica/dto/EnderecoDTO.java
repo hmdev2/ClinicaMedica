46e00ecdcafd1eae68ee442c6328725152034c8c
hmdev2 <hmdev2@proton.me>
2026-06-02 11:11:59 -0300

    chore: Completa EnderecoDTO

--- src/main/java/clinica/dto/EnderecoDTO.java
+++ src/main/java/clinica/dto/EnderecoDTO.java
@@ -1,21 +1,72 @@
 package clinica.dto;
 
 public class EnderecoDTO {
-    private String endereco;
-    
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
     public EnderecoDTO() {
     }
-
-    public EnderecoDTO(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
     
-    @Override
-    public String toString() {
-        return endereco;
-    } 
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
